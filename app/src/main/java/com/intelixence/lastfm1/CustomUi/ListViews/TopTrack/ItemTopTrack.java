4891adf39e2c0abc3793f1a8ce0a05268cd5ff54
Julian Ramirez <xprite.g@gmail.com>
2020-04-23 12:56:30 -0500

    Changes - Add rank to item top track

---
+++ app/src/main/java/com/intelixence/lastfm1/CustomUi/ListViews/TopTrack/ItemTopTrack.java
@@ -0,0 +1,12 @@
+package com.intelixence.lastfm1.CustomUi.ListViews.TopTrack;
+
+import android.graphics.Bitmap;
+
+public class ItemTopTrack {
+
+    int rank_track;
+    String track_name, track_duration, track_url, artist_name, artist_url;
+    int track_listeners;
+    Bitmap bm_image;
+
+}
---
+++ app/src/main/java/com/intelixence/lastfm1/CustomUi/ListViews/TopTrack/ListViewTopTrack.java
@@ -0,0 +1,4 @@
+package com.intelixence.lastfm1.CustomUi.ListViews.TopTrack;
+
+public class ListViewTopTrack {
+}
